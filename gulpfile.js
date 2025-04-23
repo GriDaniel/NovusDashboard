49ae5616a5743139a20380c3e742e125f452237b
GriDaniel <90074031+GriDaniel@users.noreply.github.com>
2025-04-23 14:31:12 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,128 +1,82 @@
-﻿const gulp = require('gulp');
+const gulp = require('gulp');
 const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
 const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
-const beautify = require('gulp-beautify');
-const rename = require('gulp-rename');
+const merge = require('merge-stream');
+const fs = require('fs');
 const path = require('path');
 
-const paths = {
-    elementManagers: {
-        src: 'wwwroot/js/ProductionHistory/ElementManagers/**/*.js',
-        dest: 'wwwroot/dist/js/productionhistory/elementmanagers/',
-        bundleName: 'element-managers.js'
-    },
-    modules: {
-        src: 'wwwroot/js/ProductionHistory/Modules/*.js',
-        dest: 'wwwroot/dist/js/productionhistory/modules/',
-        bundleName: 'modules.js'
-    },
-    utilities: {
-        src: 'wwwroot/js/ProductionHistory/Utilities/*.js',
-        dest: 'wwwroot/dist/js/productionhistory/utilities/',
-        bundleName: 'utilities.js'
-    },
-    index: {
-        src: 'wwwroot/js/ProductionHistory/Index.js',
-        dest: 'wwwroot/dist/js/productionhistory/',
-        bundleName: 'index.js'
-    },
-    minifiedFiles: 'wwwroot/dist/js/**/*.min.js',
-    unminifiedOutput: 'wwwroot/dist/unminified'
-};
+// Helper function to get parent folders
+function getParentFolders(srcPath) {
+    return fs.readdirSync(srcPath)
+        .filter(f => fs.statSync(path.join(srcPath, f)).isDirectory());
+}
 
-// Clean dist folders
-function cleanDist() {
+// Development CSS: Concatenate without minification
+gulp.task('development-css', () => {
+    const parentFolders = getParentFolders('src/css');
+    const streams = parentFolders.map(folder => {
+        return gulp.src(`src/css/${folder}/**/*.css`)
+            .pipe(concat(`${folder}.css`))
+            .pipe(gulp.dest('wwwroot/css'));
+    });
+    return merge(streams);
+});
+
+// Development JS: Concatenate without minification
+gulp.task('development-js', () => {
+    const parentFolders = getParentFolders('src/js');
+    const streams = parentFolders.map(folder => {
+        return gulp.src(`src/js/${folder}/**/*.js`)
+            .pipe(concat(`${folder}.js`))
+            .pipe(gulp.dest('wwwroot/js'));
+    });
+    return merge(streams);
+});
+
+// Combined development task
+gulp.task('development', gulp.parallel('development-css', 'development-js'));
+
+// Production CSS: Concatenate and minify
+gulp.task('production-css', () => {
+    const parentFolders = getParentFolders('src/css');
+    const streams = parentFolders.map(folder => {
+        return gulp.src(`src/css/${folder}/**/*.css`)
+            .pipe(concat(`${folder}.min.css`))
+            .pipe(cssmin())
+            .pipe(gulp.dest('wwwroot/css'));
+    });
+    return merge(streams);
+});
+
+// Production JS: Concatenate and minify
+gulp.task('production-js', () => {
+    const parentFolders = getParentFolders('src/js');
+    const streams = parentFolders.map(folder => {
+        return gulp.src(`src/js/${folder}/**/*.js`)
+            .pipe(concat(`${folder}.min.js`))
+            .pipe(uglify())
+            .pipe(gulp.dest('wwwroot/js'));
+    });
+    return merge(streams);
+});
+
+// Clean non-minified files in production
+gulp.task('clean-non-minified', () => {
     return del([
-        'wwwroot/dist/js/**/*.js',
-        'wwwroot/dist/js/**/*.map'
+        'wwwroot/css/**/*.css', '!wwwroot/css/**/*.min.css',
+        'wwwroot/js/**/*.js', '!wwwroot/js/**/*.min.js'
     ]);
-}
+});
 
-// Process and bundle ElementManagers
-function processElementManagers() {
-    return gulp.src(paths.elementManagers.src)
-        .pipe(sourcemaps.init())
-        .pipe(concat(paths.elementManagers.bundleName))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.elementManagers.dest));
-}
+// Combined production task
+gulp.task('production', gulp.series(
+    gulp.parallel('production-css', 'production-js'),
+    'clean-non-minified'
+));
 
-// Process and bundle Modules
-function processModules() {
-    return gulp.src(paths.modules.src)
-        .pipe(sourcemaps.init())
-        .pipe(concat(paths.modules.bundleName))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.modules.dest));
-}
-
-// Process and bundle Utilities
-function processUtilities() {
-    return gulp.src(paths.utilities.src)
-        .pipe(sourcemaps.init())
-        .pipe(concat(paths.utilities.bundleName))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.utilities.dest));
-}
-
-// Process Index
-function processIndex() {
-    return gulp.src(paths.index.src)
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename({
-            basename: 'index',
-            suffix: '.min'
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.index.dest));
-}
-
-function watch() {
-    gulp.watch(paths.elementManagers.src, processElementManagers);
-    gulp.watch(paths.modules.src, processModules);
-    gulp.watch(paths.utilities.src, processUtilities);
-    gulp.watch(paths.index.src, processIndex);
-}
-
-function unminifyFiles() {
-    return gulp.src(paths.minifiedFiles)
-        .pipe(beautify({ indent_size: 2 }))
-        .pipe(rename((file) => {
-            file.basename = file.basename.replace('.min', '');
-            file.extname = '.unmin.js';
-        }))
-        .pipe(gulp.dest(paths.unminifiedOutput));
-}
-
-
-
-// Build task for individual category bundles
-const buildCategories = gulp.parallel(
-    processElementManagers,
-    processModules,
-    processUtilities,
-    processIndex
-);
-
-// Build task
-const build = gulp.series(
-    cleanDist,
-    buildCategories
-);
-
-// Define tasks
-exports.clean = cleanDist;
-exports.buildCategories = buildCategories;
-exports.build = build;
-exports.watch = watch;
-exports.default = build;
-exports.unminify = unminifyFiles;
+// Watch task for development
+gulp.task('watch', () => {
+    gulp.watch('src/**/*', gulp.series('development'));
+});
