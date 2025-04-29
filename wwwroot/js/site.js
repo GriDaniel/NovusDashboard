e23c863ee504c1fca48c137fa8882e7555ff9748
GriDaniel <90074031+GriDaniel@users.noreply.github.com>
2025-04-29 06:16:25 -0400

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,20 +5,43 @@
 
 
 function loadSection(section) {
-    $('#dashboard-container').html('<div>Loading...</div>');
+    // Remove old section-specific CSS and JS
+    $('link[data-view], script[data-view]').remove();
 
+    // Load the section
     $.ajax({
-        url: '/' + section + '/Index',
+        url: '/Dashboard/Get' + section, // Calls the controller action
         type: 'GET',
         success: function(data) {
-            $('#dashboard-container').html(data);
-            window.location.hash = section;
-            initializeSection(section);
+            $('#dashboard-container').html(data); // Puts the section in the container
+            loadViewResources(section); // Loads CSS and JS for this section
         },
         error: function() {
-            $('#dashboard-container').html('<div>Error loading section.</div>');
+            $('#dashboard-container').html('<p>Error loading section.</p>');
         }
     });
+}
+
+function loadViewResources(section) {
+    // Add CSS for this section
+    var cssLink = $('<link>', {
+        rel: 'stylesheet',
+        href: '/css/Views/' + section + '/' + section.toLowerCase() + '.css',
+        'data-view': section
+    });
+    $('head').append(cssLink);
+
+    // Add JS for this section
+    $.getScript('/js/Views/' + section + '/' + section.toLowerCase() + '.js')
+        .done(function() {
+            console.log(section + ' JS loaded');
+            if (typeof window['init' + section] === 'function') {
+                window['init' + section](); // Runs an init function if it exists
+            }
+        })
+        .fail(function() {
+            console.log('No JS for ' + section); // Okay if there’s no JS file
+        });
 }
 
 function initializeSection(section) {
